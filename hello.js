3b8d3740f1f2feff9d45a6baf956b3e5d764f847
GanjikuntaNikhitha <nikhitha.ganjigunta62@gmail.com>
2020-06-02 16:26:02 +0530

    varibles,statements,loop

---
+++ hello.js
@@ -0,0 +1,58 @@
+console.log("hello world");
+// variables
+let name = "12345.000"
+name = "unkonwn";
+
+let amount = 1000;
+amount = 500;
+console.log(amount)
+console.log("My name is " + name)
+
+
+const my = "nikkk";
+// fName = "vasu"
+console.log("My name is " + my)
+
+let firstName = "vasu"
+
+let secondName = "Gona"
+console.log("My name is " + firstName + " " + secondName)
+console.log("My name is " + firstName  + secondName)
+// ternary operator
+var age = 20
+var isAdult = (age < 18) ? "Too young": "Old enough";
+console.log(isAdult)
+// if eelse if else
+if(age<18){
+    console.log("Too young")
+}
+else if ( age == 13){
+    console.log("Old enough")
+}
+else {
+    console.log("new records")
+}
+
+// if 
+if(age >18){
+    console.log("Too if state ment")
+}
+// switch
+var day = 3;
+switch (day) {
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wednesday")
+    break;
+  default:
+    console.log("Another day")
+}
+// for loop 
+for (i=1; i<=5; i++) {
+   console.log(i);
+ }
